5182af8c4c66cfff4467927122bf11ac33befdbf
Prerna RAJJOO <prer.rajjoo@gmail.com>
2026-05-05 21:26:10 +0800

    Set up Maven with JavaFx and SQLite dependencies

---
+++ src/main/java/com/inventory/Main.java
@@ -0,0 +1,26 @@
+package com.inventory;
+
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Button btn = new Button("Hello Inventory");
+
+        StackPane root = new StackPane(btn);
+
+        Scene scene = new Scene(root, 600, 400);
+
+        stage.setTitle("Inventory Management System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
